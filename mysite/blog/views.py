30bd58f60faa5a2ae3801becf7706683b1d3b9a7
Vincent <Vincentbelt@live.nl>
2026-01-23 09:41:19 +0100

    nieuw inladen

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from django.utils import timezone
-from .models import post
+from .models import Post
 
 def post_list (request):
-    post = Post.objects.filter(published_date__1te=timezone.now()).order_by('published_date')
+    posts = Post.objects.filter(published_date__1te=timezone.now()).order_by('published_date')
     return render (request, 'blog/post_list.html', {'posts': posts})
 
