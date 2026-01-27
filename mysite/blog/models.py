7e1e1f65355ee2346b250d8de60dddc62f13c1b4
Vincent <Vincentbelt@live.nl>
2026-01-27 09:01:26 +0100

    aanpassingen met Django6

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -2,10 +2,10 @@ from django.conf import settings
 from django.db import models
 from django.utils import timezone
 
-class Post(models.Model):   
+class voetballers(models.Model):   
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    title = models.CharField(max_length=200)
-    text = models.TextField()
+    voetballer = models.CharField(max_length=20)
+    club = models.TextField(max_length=20)
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
 
