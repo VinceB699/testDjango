46369a4340c51a61fc373b1ba7ed7c61b74f9806
Vincent <Vincentbelt@live.nl>
2026-02-02 09:04:30 +0100

    Les 6 poll aanpassing

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views
+from django.conf.urls import include
+
 
 urlpatterns =[ 
-    path('', views.post_list, name='post_list'),   path('post/<int:pk>/', views.post_detail, name='post_inhoud'),
+    path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_inhoud'),
+    path('post/nieuw/', views.post_nieuw, name='post_nieuw'),
+    path('polls/', include('polls.urls')),
     ]
